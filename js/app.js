08b5f712c11e55e6d5068f7bd997d0f0b5bad855
jonathan-crepeau <jon.crepeau@gmail.com>
2022-06-30 16:16:45 -0700

    Chapter 9 complete - Updated Gandalf

--- js/app.js
+++ js/app.js
@@ -200,6 +200,8 @@ const forgeTheFellowShip = () => {
 // SECTION Chapter 9
 // ============
 const theBalrog = () => {
+  console.log('Chapter 9 - Update Gandalf');
+  $('div ul li.buddy').eq(0).html('Gandalf the White').attr('class', 'the-white');
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
   // 2. add a class "the-white" to this element
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
